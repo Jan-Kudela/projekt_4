5aaa126c3e9f2d8fb404c821e68d24e5b2e94040
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-09-04 22:48:24 +0200

    def hlavni_menu

---
+++ main.py
@@ -0,0 +1,21 @@
+ukoly = []
+
+def hlavni_menu():
+    print("Správce úkolů - Hlavní menu\n")
+    print("1.Přidat nový úkol")
+    print("2. Zobrait všechny úkoly")
+    print("3. Odstranit úkol")
+    print("Konec programu")
+    choice_number = input("Vyberte možnost (1-4):")
+
+hlavni_menu()
+
+def pridat_ukol():
+    task_name = input("Zadejte název úkolu:")
+    task_cont = input("Zadejte popis úkolu:")
+    
+    ukol_pridani=()
+    ukol_pri
+
+pridat_ukol()
+print(ukoly)
