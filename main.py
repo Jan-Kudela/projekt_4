e5310e81b4e066c0b3b78fadfe04d0736938aafa
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-09-08 21:35:10 +0200

    Doplněno ošetření vstupů tak, aby byla zadána správná číselná hodnota

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 ukoly = []
 
+def pridat_ukol():
+    while True:
+        task_name = input("Zadejte název úkolu:")
+        task_cont = input("Zadejte popis úkolu:")
+        
+        if not task_name or not task_cont:
+            print("Název úkolu i jeho popis musí být vyplněny.")
+            continue
+       
+        else:
+            ukol_pridani = (task_name, task_cont)
+            ukoly.append(ukol_pridani)
+            print("Úkol přidán.\n")
+            break
+
+def zobrazit_ukoly():
+    for ukol,zadani in ukoly:
+        print(f"{ukol} - {zadani}\n")
+
+
+
 def hlavni_menu():
-    print("Správce úkolů - Hlavní menu\n")
-    print("1.Přidat nový úkol")
-    print("2. Zobrait všechny úkoly")
-    print("3. Odstranit úkol")
-    print("Konec programu")
-    choice_number = input("Vyberte možnost (1-4):")
+    while True:
+        print("Správce úkolů - Hlavní menu\n")
+        print("1. Přidat nový úkol")
+        print("2. Zobrait všechny úkoly")
+        print("3. Odstranit úkol")
+        print("4. Konec programu")
+        while True:
+            choice_number = (input("Vyberte možnost (1-4):"))
+            if choice_number.isdigit():
+                choice_number = int(choice_number)
+                if choice_number not in range(1,5):
+                    print("Zadané číslo musí být v rozsahu 1-4")
+                else:
+                    break
+            else:
+                print("Zadaná hodnota musí být číslo v rozsahu 1-4")
+                continue
+
+        
+        if choice_number == 1:
+            pridat_ukol()
+        elif choice_number == 2:
+            zobrazit_ukoly()
+
 
 hlavni_menu()
-
-def pridat_ukol():
-    task_name = input("Zadejte název úkolu:")
-    task_cont = input("Zadejte popis úkolu:")
-    if not task_name or not task_cont:
-        print("Název a popis úkolu nesmí být prázdné.")
-        return
-    else:
-        ukol_pridani = (task_name, task_cont)
-        ukoly.append(ukol_pridani)
-        print("Úkol přidán.")
-
-pridat_ukol()
-print(ukoly)
