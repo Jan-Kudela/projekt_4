947360683b4d0e04fe12df2271148f0499961436
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-09-08 21:57:09 +0200

    dopsána funkce odstranit_ukol

--- main.py
+++ main.py
@@ -16,16 +16,31 @@ def pridat_ukol():
             break
 
 def zobrazit_ukoly():
-    for ukol,zadani in ukoly:
-        print(f"{ukol} - {zadani}\n")
+    for index, ukol in enumerate(ukoly):
+        print(f"Seznam úkolů:\n{index+1}. {ukol[0]} - {ukol[1]}")
 
+
+def odstranit_ukol():
+    zobrazit_ukoly()
+    while True:
+        number_del = input("Zadejte číslo úkolu, který chcete odstranit: ")
+        if number_del.isdigit():
+            number_del = int(number_del)
+            break
+        else:
+            print(f"Zadaná hodnota musí být číslo v rozsahu {len(ukoly)}.")
+            continue
+    
+    print(f"Úkol {ukoly[number_del-1]} byl odstraněn.")
+    ukoly.pop(number_del-1)        
+    
 
 
 def hlavni_menu():
     while True:
         print("Správce úkolů - Hlavní menu\n")
         print("1. Přidat nový úkol")
-        print("2. Zobrait všechny úkoly")
+        print("2. Zobrazit všechny úkoly")
         print("3. Odstranit úkol")
         print("4. Konec programu")
         while True:
@@ -45,6 +60,7 @@ def hlavni_menu():
             pridat_ukol()
         elif choice_number == 2:
             zobrazit_ukoly()
-
+        elif choice_number == 3:
+            odstranit_ukol()
 
 hlavni_menu()
