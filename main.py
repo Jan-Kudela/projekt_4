b3e3db862372e0e690e6d1fe2e8f5cbbe690b6b0
Jan-Kudela <jochanan.jorgen@gmail.com>
2025-09-08 20:00:09 +0200

    opravena funkce pridat_ukol,

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ hlavni_menu()
 def pridat_ukol():
     task_name = input("Zadejte název úkolu:")
     task_cont = input("Zadejte popis úkolu:")
-    
-    ukol_pridani=()
-    ukol_pri
+    if not task_name or not task_cont:
+        print("Název a popis úkolu nesmí být prázdné.")
+        return
+    else:
+        ukol_pridani = (task_name, task_cont)
+        ukoly.append(ukol_pridani)
+        print("Úkol přidán.")
 
 pridat_ukol()
 print(ukoly)
